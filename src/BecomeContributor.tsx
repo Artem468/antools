41bf2d05bf7a3395975ce87fd499e5651b16bd39
Artem468 <artem.belykh.ru@gmail.com>
2025-01-27 01:14:50 +0200

    Vova blyat nu eto pizdec, I tvoi shablonov rot ebal

--- src/BecomeContributor.tsx
+++ src/BecomeContributor.tsx
@@ -5,10 +5,14 @@ function BecomeContributor() {
         <section className="BecomeContributor">
             <p className="Title">Become a contributor?</p>
             <p className="Description">Join us and get tips & tricks to become a great Designer and Developer</p>
-            <div className="BecomeContributorInput">
-                <input placeholder="Enter your email..."/>
-                <button>Join Us</button>
-            </div>
+            <form method="POST" className="BecomeContributorInput">
+                <input type="text" placeholder="Enter your name..."/>
+                <input type="tel" placeholder="Enter your phone..."/>
+                <input type="email" placeholder="Enter your email..."/>
+                <button type="submit">Join Us</button>
+            </form>
+
+
         </section>
     )
 }
